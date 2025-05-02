8d729d6f2f66fb12361df102bb5243ab90800832
Mike K <meik@ddr.live>
2025-05-02 01:33:15 -0400

    Update HeaderLinks.js
    Hide header info

--- src/components/Header/HeaderLinks.js
+++ src/components/Header/HeaderLinks.js
@@ -45,16 +45,16 @@ export const HeaderLinks = (props) => {
   return (
     <List className={classes.list}>
       
-      <ListItem className={classes.listItem}>
-        <Button
-          color='transparent'
-          onClick={() => {
-            executeScroll(servicesRef)
-          }}
-          className={classes.navLink}>
-          Our Services
-        </Button>
-      </ListItem>
+      // <ListItem className={classes.listItem}>
+      //   <Button
+      //     color='transparent'
+      //     onClick={() => {
+      //       executeScroll(servicesRef)
+      //     }}
+      //     className={classes.navLink}>
+      //     Our Services
+      //   </Button>
+      // </ListItem>
       {recentWorkRef && (
         <ListItem className={classes.listItem}>
           <Button
@@ -68,26 +68,26 @@ export const HeaderLinks = (props) => {
         </ListItem>
       )}
       
-      <ListItem className={classes.listItem}>
-        <Button
-          color='transparent'
-          onClick={() => {
-            executeScroll(aboutUsRef)
-          }}
-          className={classes.navLink}>
-          About Us
-        </Button>
-      </ListItem>
-      <ListItem className={classes.listItem}>
-        <Button
-          color='transparent'
-          onClick={() => {
-            executeScroll(teamRef)
-          }}
-          className={classes.navLink}>
-          Meet the Team
-        </Button>
-      </ListItem>
+      // <ListItem className={classes.listItem}>
+      //   <Button
+      //     color='transparent'
+      //     onClick={() => {
+      //       executeScroll(aboutUsRef)
+      //     }}
+      //     className={classes.navLink}>
+      //     About Us
+      //   </Button>
+      // </ListItem>
+      // <ListItem className={classes.listItem}>
+      //   <Button
+      //     color='transparent'
+      //     onClick={() => {
+      //       executeScroll(teamRef)
+      //     }}
+      //     className={classes.navLink}>
+      //     Meet the Team
+      //   </Button>
+      // </ListItem>
       <ListItem className={classes.listItem}>
         <Button
           color='transparent'
@@ -110,21 +110,21 @@ export const HeaderLinks = (props) => {
           </Button>
         </ListItem>
       )}
-      <ListItem className={classes.listItem}>
-        <Tooltip
-          id='instagram-tooltip'
-          title='Follow us on Instagram'
-          placement={window.innerWidth > 959 ? 'top' : 'left'}
-          classes={{ tooltip: classes.tooltip }}>
-          <Button
-            color='transparent'
-            href='https://www.instagram.com/ddr_live/'
-            target='_blank'
-            className={classes.navLink}>
-            <InstagramIcon />
-          </Button>
-        </Tooltip>
-      </ListItem>
+      // <ListItem className={classes.listItem}>
+      //   <Tooltip
+      //     id='instagram-tooltip'
+      //     title='Follow us on Instagram'
+      //     placement={window.innerWidth > 959 ? 'top' : 'left'}
+      //     classes={{ tooltip: classes.tooltip }}>
+      //     <Button
+      //       color='transparent'
+      //       href='https://www.instagram.com/ddr_live/'
+      //       target='_blank'
+      //       className={classes.navLink}>
+      //       <InstagramIcon />
+      //     </Button>
+      //   </Tooltip>
+      // </ListItem>
       
     </List>
   )
